7efbea726ae652c5efe348ea20f247e4c775b586
iamnikitakostin <141674557+iamnikitakostin@users.noreply.github.com>
2024-06-03 18:14:02 -0700

    speed optimization

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 import "./Header.css"
 
-import images from '../../constants/images';
 import { useTranslation } from 'react-i18next';
+import ramen from "../../assets/ramen_header.jpg";
+import ramenSm from "../../assets/ramen_header-sm.jpeg";
 
 function Header() {
   const [t] = useTranslation("global");
@@ -13,9 +14,10 @@ function Header() {
         <h1 className="headtext__tenor">{t("banner.title")}</h1>
         <p className="p__opensans" style={{ margin: "0 0 1rem 0", color: "var(--color-lightSilver)"}}>{t("banner.subtitle")}</p>
       </div>
-      <div className='app__wrapper_img'>
-        <img src={images.header01} alt="ramen header" decoding='async'/>
-      </div>
+        <picture className='app__wrapper_img'>
+          <source srcSet={ramen} alt="ramen" media='(min-width: 600px)'/>
+          <img src={ramenSm} alt="ramen" height='270' width='270' loading='lazy' decoding='async' />
+        </picture>
     </div>
   )
 }
