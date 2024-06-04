0e512c870b28ed6b4b23946a5974ac3c41b4318a
iamnikitakostin <141674557+iamnikitakostin@users.noreply.github.com>
2024-06-03 17:19:44 -0700

    speed optimization

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -10,11 +10,11 @@ function Header() {
   return (
     <div className='app__header app__wrapper section__padding' id='home'>
       <div className='app__wrapper_info'>
-        <h1 className="headtext__tenor" style={{fontFamily: "serif", lineHeight: "117px"}}>{t("banner.title")}</h1>
+        <h1 className="headtext__tenor" style={{lineHeight: "117px"}}>{t("banner.title")}</h1>
         <p className="p__opensans" style={{ margin: "0 0 1rem 0", color: "var(--color-lightSilver)"}}>{t("banner.subtitle")}</p>
       </div>
       <div className='app__wrapper_img'>
-        <img src={images.header01} alt="ramen header" />
+        <img src={images.header01} alt="ramen header" decoding='async'/>
       </div>
     </div>
   )
